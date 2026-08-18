c7ec05ecb23160b3ee18f38d9b0f0566f6a20f6c
Dominik <65778271+1ordNero@users.noreply.github.com>
2026-08-18 21:13:05 +0300

    Route Forgotten Forest game and editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ForestEditor from './ForestEditor'
+import ForestGame from './ForestGame'
 import './styles/global.css'
 import './styles/editor.css'
 import './styles/phase2.css'
 
+const params = new URLSearchParams(window.location.search)
+const forestEditor = params.get('editor') === '1' && params.get('scene') === 'forest'
+const forestGame = params.get('forest') === '1'
+const Root = forestEditor ? ForestEditor : forestGame ? ForestGame : App
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
